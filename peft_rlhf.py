c6ff2b13be6ed1a6e734f706499d5e4f14f2f5e8
rqchao <rqchao@berkeley.edu>
2023-12-06 10:32:36 -0800

    rlhf

--- peft_rlhf.py
+++ peft_rlhf.py
@@ -1,17 +1,3 @@
-# coding=utf-8
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -163,10 +149,10 @@ ppo_trainer = PPOTrainer(
     optimizer=optimizer,
 )
 
-# We then build the reward pipeline, we will use the toxicity model to compute the reward.
-# We first load the toxicity model and tokenizer.
+# We then build the reward pipeline, we will use the personality model to compute the reward.
+# We first load the personality model and tokenizer.
 tokenizer = BertTokenizer.from_pretrained("Minej/bert-base-personality")
-# We load the toxicity model in fp16 to save memory.
+# We load the personality model in fp16 to save memory.
 toxicity_model = BertForSequenceClassification.from_pretrained("Minej/bert-base-personality", torch_dtype=torch.int8).to(
     ppo_trainer.accelerator.device
 )
